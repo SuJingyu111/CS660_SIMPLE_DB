b1a8fe9bb90308b7bc9e39294abeb1267bf0473c
troyyxk <39604949+troyyxk@users.noreply.github.com>
2021-10-29 01:55:33 -0400

    Fix deleteparententry but

--- src/java/simpledb/BTreeFile.java
+++ src/java/simpledb/BTreeFile.java
@@ -875,7 +875,7 @@ public class BTreeFile implements DbFile {
 		setEmptyPage(tid, dirtypages, rightPage.getId().pageNumber());
 
 		// delete parent entry
-		deleteParentEntry(tid, dirtypages, rightPage, parent, parentEntry);
+		deleteParentEntry(tid, dirtypages, leftPage, parent, parentEntry);
 
 	}
 
